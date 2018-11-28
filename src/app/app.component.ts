8b37ece94f8628180dc850ade3d6a9e2cb9299eb
IIPL\13596 <mohit.saxena@compunnel.in>
2018-11-28 13:06:17 +0530

    added code for spread sheet design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'spreadsheetapp';
+  charArray: Array<string>;
+  numberArray: Array<string>;
+  selectedColumnIndex: number;
+  selectedRowIndex: number;
+  totalNumberOfRows: number = 50;
+
+  ngOnInit() {
+    let chars: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.charArray = chars.split('');
+    this.charArray.unshift(''); // push '' on first index because we need first column as blank
+
+    this.numberArray = (<any>Array(this.totalNumberOfRows)).fill().map((x, i) => i + 1);
+    this.numberArray.unshift(''); // push '' on first index because we need first column as blank
+
+  }
+
+  /**
+   * 
+   * @param rowIndex - it is row Index where I have focused on Input Field
+   * @param colIndex  - it is column Index where I have focused on Input Field
+   */
+  onInputFocus(rowIndex: number, colIndex: number): void {
+    this.selectedColumnIndex = colIndex;
+    this.selectedRowIndex = rowIndex;
+  }
+
 }
